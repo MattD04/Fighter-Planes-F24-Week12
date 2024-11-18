f2b7157986af3c830ab41ddb0db2e2d5fcebe288
MattD04 <118762220+MattD04@users.noreply.github.com>
2024-11-17 20:20:59 -0500

    Adding Asset Folder

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using TMPro;
 
 public class GameManager : MonoBehaviour
@@ -11,11 +12,21 @@ public class GameManager : MonoBehaviour
     public GameObject enemy;
     public GameObject cloud;
     public GameObject coin;
+    public GameObject powerup;
+
+    public AudioClip powerUp;
+    public AudioClip powerDown;
+
+    private bool isPlayerAlive;
+
     private int score;
     private int lives;
 
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI livesText;
+    public TextMeshProUGUI gameOverText;
+    public TextMeshProUGUI restartText;
+    public TextMeshProUGUI PowerUpText;
 
     // Start is called before the first frame update
     void Start()
@@ -23,22 +34,33 @@ public class GameManager : MonoBehaviour
         Instantiate(player, transform.position, Quaternion.identity);
         InvokeRepeating("CreateEnemy", 1f, 3f);
         InvokeRepeating("CreateCoin", 2f, 5f);
+        // Method with a waiting function
+        StartCoroutine(CreatePowerup());
         CreateSky();
         score = 0;
         lives = 3;
         scoreText.text = "Score: " + score;
         livesText.text = "Lives: " + lives;
+        isPlayerAlive = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        Restart();
     }
 
     void CreateEnemy()
     {
         Instantiate(enemy, new Vector3(Random.Range(-9f, 9f), 7.5f, 0), Quaternion.identity);
+    }
+
+    // Creates a powerup at start, and picks a random range within 2 and 5 seconds and loops back
+    IEnumerator CreatePowerup()
+    {
+        Instantiate(powerup, new Vector3(Random.Range(-9, 9f),7.5f, 0), Quaternion.identity); //Creates Powerup
+        yield return new WaitForSeconds(Random.Range(3f, 6f));
+        StartCoroutine(CreatePowerup()); // Restarts
     }
     void CreateCoin()
     {
@@ -61,5 +83,36 @@ public class GameManager : MonoBehaviour
     {
         lives--;
         livesText.text = "Lives: " + lives; // Update the UI text
+    }
+
+    public void GameOver()
+    {
+        isPlayerAlive = false;
+        CancelInvoke();
+        gameOverText.gameObject.SetActive(true);
+        restartText.gameObject.SetActive(true);
+    }
+
+    void Restart()
+    {
+        if(Input.GetKeyDown(KeyCode.R) && isPlayerAlive == false)
+        {
+            SceneManager.LoadScene("Game");
+            //Restarts Game
+        }
+    }
+
+    public void UpdatePowerupText(string whichPowerup)
+    {
+        PowerUpText.text = whichPowerup;
+    }
+
+    public void PlayPowerUp()
+    {
+        AudioSource.PlayClipAtPoint(powerUp, Camera.main.transform.position);
+    }
+    public void PlayPowerDown()
+    {
+        AudioSource.PlayClipAtPoint(powerDown, Camera.main.transform.position);
     }
 }
--- Assets/Scripts/MovingObject.cs
+++ Assets/Scripts/MovingObject.cs
@@ -27,6 +27,11 @@ public class MovingObject : MonoBehaviour
         {
             //I am a cloud
             transform.Translate(new Vector3(0, -1, 0) * Time.deltaTime * Random.Range(2f, 6f));
+        } else if (myType == 4)
+        {
+            //I am a powerup
+            transform.Translate(new Vector3(0, -1, 0) * Time.deltaTime * 6f);
+
         }
 
         if ((transform.position.y > 9f ||  transform.position.y <= -9f) && myType != 3)
